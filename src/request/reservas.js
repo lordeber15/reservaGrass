6fb8cf0131b595164c480e4d165c4dc1be79ab76
lordeber15 <eberpalomino@hotmail.com>
2025-06-05 12:39:09 -0500

    para despliegue

--- src/request/reservas.js
+++ src/request/reservas.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const reservasApi = axios.create({
-  baseURL: "https://backendreservagrass-production.up.railway.app/",
+  baseURL:
+    "https://grass-hacienda-grass-hacienda-sq1c0u-0533db-31-97-28-201.traefik.me/",
 });
 export const getReservas = async () => {
   const res = await reservasApi.get("/reservas");
